5de99459817fa5ef56f5db30ce9b879a5831bcd1
shecannotsee <shecannotsee@163.com>
2023-01-06 00:46:28 +0800

    feat:添加递归测试的测试代码

--- test/method_2_RecordingTime.h
+++ test/method_2_RecordingTime.h
@@ -24,6 +24,24 @@ void main() {
   std::cout<<time_record_se.get()<<"s\n";
   std::cout<<time_record_ms.get()<<"ms\n";
   std::cout<<time_record_us.get()<<"us\n";
+
+  // 递归测试
+  sheMonitor::RecordingTime recursion_se(sheMonitor::TimeUnit::Seconds);
+  sheMonitor::RecordingTime recursion_ms(sheMonitor::TimeUnit::Milliseconds);
+  sheMonitor::RecordingTime recursion_us(sheMonitor::TimeUnit::Microseconds);
+  recursion_se.start();
+  recursion_ms.start();
+  recursion_us.start();
+  for(int i=0; i<time_record_us.get(); i++) {
+    time_record_se.start();
+  };
+  recursion_se.end();
+  recursion_ms.end();
+  recursion_us.end();
+  std::cout<<(double)recursion_se .get()<<"s\n";
+  std::cout<<(double)recursion_ms.get()/1000<<"s\n";
+  std::cout<<(double)recursion_us.get()/1000000<<"s\n";
+
 };
 
 };// namespace method_2_RecordingTiome
