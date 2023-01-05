972dd4d7840cc3b18f58d0eb02b08ffb3c2d759c
shecannotsee <shecannotsee@163.com>
2023-01-06 00:29:35 +0800

    feat:修改文件名

---
+++ src/Recording/time_unit.h
@@ -0,0 +1,34 @@
+//
+// Created by  shecannotsee on 2023/1/5.
+//
+
+#ifndef SHE_MONITOR_TIME_UNIT_H
+#define SHE_MONITOR_TIME_UNIT_H
+
+#include <chrono>
+
+namespace sheMonitor {
+
+namespace bit32 {
+  using Microseconds = std::chrono::duration<int, std::ratio<1, 1000000>>;
+  using Milliseconds = std::chrono::duration<int, std::ratio<1, 1000>>;
+  using Seconds = std::chrono::duration<int, std::ratio<1, 1>>;
+};// namespace bit32
+
+namespace bit64{
+  using Microseconds = std::chrono::duration<long long int, std::ratio<1, 1000000>>;
+  using Milliseconds = std::chrono::duration<long long int, std::ratio<1, 1000>>;
+  using Seconds = std::chrono::duration<long long int, std::ratio<1, 1>>;
+};// namespace bit64
+
+enum class TimeUnit : unsigned int {
+  Microseconds = 0, // 微秒
+  Milliseconds,     // 毫秒
+  Seconds           // 秒
+};
+
+
+
+};// namespace sheMonitor
+
+#endif //SHE_MONITOR_TIME_UNIT_H
