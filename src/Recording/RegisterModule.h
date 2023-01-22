746afea9c561adea1767585e40298eec11d049bb
shecannotsee <shecannotsee@163.com>
2023-01-22 14:19:06 +0800

    style:修改格式

--- src/Recording/RegisterModule.h
+++ src/Recording/RegisterModule.h
@@ -5,21 +5,24 @@
 #ifndef SHE_MONITOR_REGISTERMODULE_H
 #define SHE_MONITOR_REGISTERMODULE_H
 
+#include <Recording/RecordingTime.h>
 
+namespace sheMonitor {
 class RegisterModule {
  public:
   RegisterModule();
   ~RegisterModule();
   // Disable copy
-  RegisterModule(const RegisterModule&) = delete;
-  RegisterModule& operator=(const RegisterModule&) = delete;
+  RegisterModule(const RegisterModule &) = delete;
+  RegisterModule &operator=(const RegisterModule &) = delete;
   // Disable move
-  RegisterModule(RegisterModule&&) = delete;
-  RegisterModule& operator=(RegisterModule&&) = delete;
+  RegisterModule(RegisterModule &&) = delete;
+  RegisterModule &operator=(RegisterModule &&) = delete;
 
  private:
 
-};
+};// class RegisterModule
+};// namespace sheMonitor
 
 
 #endif //SHE_MONITOR_REGISTERMODULE_H
